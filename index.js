19c0e10369a34f4b150f1110eeafef0ddb648a32
Muhammad Iqbal Masykuri <79145548+muhiqbalm@users.noreply.github.com>
2022-09-24 16:31:10 +0700

    feat(setup): create mongodb connection

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+import process from 'process';
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
+import getenv from './src/helper/getenv.js';
 
 const app = express();
 
-const PORT = 3000;
+const PORT = getenv('PORT');
+const MONGO_URI = getenv('MONGO_URI');
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log('Connected to DocIn Database!'))
+  .catch((err) => {
+    console.error(`Can't connect to DocIn Database!`);
+    console.error(err);
+    process.exit(1);
+  });
 
 app.use(cors());
 

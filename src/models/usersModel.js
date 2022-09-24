2aceb10e4f89886c3b1f7a47210dd7a84bed4938
dindasabela <dindasabela02@mail.ugm.ac.id>
2022-09-24 17:04:56 +0700

    feat(users): create user model

---
+++ src/models/usersModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 200,
+    },
+    last_name: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 200,
+    },
+    username: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 100,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 1024
+    },
+    bio: {
+        type: String,
+        default: 'Hi, I am using Docin'
+    },
+    address: {
+        type: String,
+        default: 'Yogyakarta, Indonesia'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+module.exports = mongoose.model('User', userSchema);

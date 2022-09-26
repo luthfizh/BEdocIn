b3d229f513008c33ac33d95b158fd3b1fb1feaa7
Muhammad Iqbal Masykuri <79145548+muhiqbalm@users.noreply.github.com>
2022-09-26 17:18:20 +0700

    feat(appointments): add appointment schema

---
+++ src/models/appointmentsModel.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const appointmentSchema = new mongoose.Schema({
+    creator_id: {
+        type: String,
+        required: true,
+    },
+    receiver_id: {
+        type: String,
+        required: true,
+    },
+    creator_name: {
+        type: String,
+        required: true,
+    },
+    receiver_name: {
+        type: String,
+        required: true,
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    explanation: {
+        type: String,
+        required: true,
+    },
+    time: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        default: "pending"
+    },
+    appointment_fee: {
+        type: Number,
+        default: 0
+    },
+    fee_status: {
+        type: String,
+        default: "unsettled"
+    }
+})
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+export default Appointment;

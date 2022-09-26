50bb864fa78f804736279286c350a0c27edd3452
luthfizh <luthfi.izzuddin1@gmail.com>
2022-09-26 14:27:54 +0700

    feat(users): add validation user controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -13,7 +13,7 @@ export const findAllUser = async (req, res, next) => {
 export const findUserById = async (req, res, next) => {
   try {
     const id = mongoose.Types.ObjectId(req.params.id);
-    const response = await User.findOne({ _id: id });
+    const response = await User.findById({ _id: id });
     res.json({ response });
   } catch (err) {
     next(err);
@@ -24,7 +24,7 @@ export const createUser = async (req, res, next) => {
   try {
     const user = new User(req.body);
     const result = await user.save();
-    res.status(201).json(result);
+    res.status(201).send({ message: "User successfully created!" });
   } catch (err) {
     if (["CastError", "ValidationError"].includes(err?.name)) {
       next({
@@ -39,11 +39,17 @@ export const createUser = async (req, res, next) => {
 
 export const updateUserById = async (req, res, next) => {
   try {
-    const response = await User.findByIdAndUpdate(
-      { _id: req.params.id },
-      req.body
-    );
-    res.json({ response });
+    const id = mongoose.Types.ObjectId(req.params.id);
+    const response = await User.findByIdAndUpdate({ _id: id }, req.body);
+    if (!response) {
+      res
+        .status(404)
+        .send({ message: `Can't update, user with id=${id} not found!` });
+    } else if (Object.keys(req.body).length === 0) {
+      res.status(404).send({ message: "Can't update, update value is empty!" });
+    } else {
+      res.status(201).send({ message: "User successfully updated!" });
+    }
   } catch (err) {
     next(err);
   }
@@ -51,8 +57,15 @@ export const updateUserById = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
   try {
-    const response = await User.findByIdAndRemove({ _id: req.params.id });
-    res.json({ response });
+    const id = mongoose.Types.ObjectId(req.params.id);
+    const response = await User.findByIdAndRemove({ _id: id });
+    if (!response) {
+      res
+        .status(404)
+        .send({ message: `Delete failed, user with id=${id} not found!` });
+    } else {
+      res.status(201).send({ message: "User successfully deleted!" });
+    }
   } catch (err) {
     next(err);
   }

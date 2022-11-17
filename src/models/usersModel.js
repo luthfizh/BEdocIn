c0cc821bd5eba8ccbf32917978f140945d1dd104
luthfizh <luthfi.izzuddin1@gmail.com>
2022-11-17 22:25:10 +0700

    feat(model): add role for user and doctor
    additional: change port number and delete .env from .gitignore

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -1,48 +1,52 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    first_name: {
-        type: String,
-        required: true,
-        min: 3,
-        max: 200,
-    },
-    last_name: {
-        type: String,
-        required: true,
-        min: 3,
-        max: 200,
-    },
-    username: {
-        type: String,
-        required: true,
-        min: 3,
-        max: 100,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-    },
-    password: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 1024
-    },
-    address: {
-        type: String,
-        default: 'Yogyakarta, Indonesia'
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+  first_name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 200,
+  },
+  last_name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 200,
+  },
+  username: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 100,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match:
+      /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 1024,
+  },
+  address: {
+    type: String,
+    default: "Yogyakarta, Indonesia",
+  },
+  role: {
+    type: String,
+    default: "User",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;

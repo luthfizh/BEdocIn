95777553fb703fe3230bf25ee1a0fc4702bd9718
Muhammad Iqbal Masykuri <luthfi.izzuddin1@gmail.com>
2022-12-10 00:48:48 +0700

    fix handler

--- index.js
+++ index.js
@@ -54,6 +54,27 @@ module.exports = (req, res) => {
 
 }
 
+exports.handler = function(event, context, callback) {
+
+  //---- other code
+  
+   //Allow CORS in header
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+  };
+  
+     if (event.httpMethod === 'OPTIONS') {
+        // To enable CORS
+        return {
+         statusCode: 200, 
+         headers,
+         body: 'success'
+       };
+   }
+  }
+
 mongoose
   .connect(MONGO_URI)
   .then(() => console.log('Connected to DocIn Database!'))

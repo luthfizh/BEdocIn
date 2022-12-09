7d6aa6965926761da914f4fd29c311462b682607
Muhammad Iqbal Masykuri <luthfi.izzuddin1@gmail.com>
2022-12-10 02:33:44 +0700

    const

--- index.js
+++ index.js
@@ -12,68 +12,7 @@ const app = express();
 
 const PORT = getenv('PORT');
 const MONGO_URI = getenv('MONGO_URI');
-
-// const allowCors = fn => async (req, res) => {
-//   res.setHeader('Access-Control-Allow-Credentials', true)
-//   // res.setHeader('Access-Control-Allow-Origin', '*')
-//   // another common pattern
-//   res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//   )
-//   if (req.method === 'OPTIONS') {
-//     res.status(200).end()
-//     return
-//   }
-//   return await fn(req, res)
-// }
-
-// const handler = (req, res) => {
-//   const d = new Date()
-//   res.end(d.toString())
-// }
-
-// module.exports = allowCors(handler)
-// module.exports = (req, res) => {
-//   //set header first to allow request or origin domain (value can be different)
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
-
-// //---- other code
-
-// //Preflight CORS handler
-//   if(req.method === 'OPTIONS') {
-//       return res.status(200).json(({
-//           body: "OK"
-//       }))
-//   }
-
-// }
-
-// exports.handler = function(event, context, callback) {
-
-//   //---- other code
-  
-//    //Allow CORS in header
-//   const headers = {
-//     'Access-Control-Allow-Origin': '*',
-//     'Access-Control-Allow-Headers': 'Content-Type',
-//     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-//   };
-  
-//      if (event.httpMethod === 'OPTIONS') {
-//         // To enable CORS
-//         return {
-//          statusCode: 200, 
-//          headers,
-//          body: 'success'
-//        };
-//    }
-//   }
+const cors = require('cors');
 
 mongoose
   .connect(MONGO_URI)

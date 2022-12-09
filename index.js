4cc43f7fcaad32e34a0453ebff634a80933f4852
Muhammad Iqbal Masykuri <luthfi.izzuddin1@gmail.com>
2022-12-10 00:42:15 +0700

    fixxxx

--- index.js
+++ index.js
@@ -36,6 +36,23 @@ const MONGO_URI = getenv('MONGO_URI');
 // }
 
 // module.exports = allowCors(handler)
+module.exports = (req, res) => {
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+//---- other code
+
+//Preflight CORS handler
+  if(req.method === 'OPTIONS') {
+      return res.status(200).json(({
+          body: "OK"
+      }))
+  }
+
+}
 
 mongoose
   .connect(MONGO_URI)
@@ -47,7 +64,6 @@ mongoose
   });
 
 app.use(cors());
-app.use(vercel.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
